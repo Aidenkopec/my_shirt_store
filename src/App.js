3b07de8a6f902e94a57e904b0e6482a5276f7675
Aidenkopec <97480728+Aidenkopec@users.noreply.github.com>
2023-06-07 09:52:38 -0600

    [Refactor] Update App component to include ShirtForm

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import ShirtForm from "./components/ShirtForm";
 
 function App() {
   return (
     <div className="App">
-
+      <ShirtForm />
     </div>
   );
 }
