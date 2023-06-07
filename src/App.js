c7dbf240707bd2a64046bf203afb18c867cf7136
Aidenkopec <97480728+Aidenkopec@users.noreply.github.com>
2023-06-07 10:19:33 -0600

    [Refactor] Update App component with gradient background and ShirtForm

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import './App.css';
 import ShirtForm from "./components/ShirtForm";
 
 function App() {
-  return (
-    <div className="App">
-      <ShirtForm />
-    </div>
-  );
+    return (
+        <div>
+            <div className='main'>
+                <div className='gradient'/>
+            </div>
+            <div className='app'>
+                <ShirtForm/>
+            </div>
+        </div>
+    );
 }
+
 
 export default App;
